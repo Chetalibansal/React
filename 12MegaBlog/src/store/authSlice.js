1158ffadb2d7ab5946e353c400a4ba8327d019c5
Chetalibansal <chetalibansal316@gmail.com>
2025-05-29 02:08:35 +0530

    feat: Implement authentication and post management features using Appwrite
    - Added AuthService for user account creation, login, and session management.
    - Created Service class for handling post CRUD operations and file uploads.
    - Developed components for authentication layout, buttons, footer, header, and post cards.
    - Integrated TinyMCE for rich text editing in post creation and editing.
    - Implemented Redux for state management of authentication status and user data.
    - Set up routing for login, signup, and post management pages.
    - Configured Tailwind CSS for styling and responsive design.
    - Added environment configuration for Appwrite integration.

---
+++ 12MegaBlog/src/store/authSlice.js
@@ -0,0 +1,44 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+    status : false,
+    userData: null
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        }
+    }
+})
+
+
+export const {login, logout} = authSlice.actions;
+
+export default authSlice.reducer; // Export the reducer to be used in the store
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 12MegaBlog/src/store/store.js
@@ -0,0 +1,11 @@
+import {configureStore} from '@reduxjs/toolkit';
+import authSlice from './authSlice'
+
+const store =  configureStore({
+    reducer:{
+        auth: authSlice,
+        // You can add more slices here as your application grows
+    }
+});
+
+export default store;
